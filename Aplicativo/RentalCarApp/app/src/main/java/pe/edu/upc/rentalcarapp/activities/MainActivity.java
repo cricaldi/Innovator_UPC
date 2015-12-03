0169df6fb4630d1bd5369b588084cbc882b93b6b
apizarroe <apizarroe13@gmail.com>
2015-12-03 01:56:23 -0500

    Avance con Material Design
    Avance con Material Design

---
+++ Aplicativo/RentalCarApp/app/src/main/java/pe/edu/upc/rentalcarapp/activities/MainActivity.java
@@ -0,0 +1,23 @@
+package pe.edu.upc.rentalcarapp.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import pe.edu.upc.rentalcarapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), UserActivity.class));
+            }
+        });
+    }
+}
