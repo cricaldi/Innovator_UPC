7b4508133c7919934c43b01d436d0b096fc15f01
apizarroe <apizarroe13@gmail.com>
2015-12-04 22:20:12 -0500

    Adicionales
    Adicionales

--- Aplicativo/RentalCarApp/app/src/main/java/pe/edu/upc/rentalcarapp/activities/CarActivity.java
+++ Aplicativo/RentalCarApp/app/src/main/java/pe/edu/upc/rentalcarapp/activities/CarActivity.java
@@ -121,7 +121,8 @@ public class CarActivity extends AppCompatActivity
         } else if (id == R.id.nav_search) {
             startActivity(new Intent(getBaseContext(), SearchCarActivity.class));
         } else if (id == R.id.nav_rents) {
-
+            finish();
+            startActivity(new Intent(getBaseContext(), RentActivity.class));
         } /*else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
